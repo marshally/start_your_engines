a42c7c3f5d326abd99128e78e41af0a50a44ce7d
Marshall Yount <marshall@codeclimate.com>
2015-08-14 16:21:08 -0500

    schema

---
+++ db/structure.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "schema_migrations" ("version" varchar(255) NOT NULL);
+CREATE UNIQUE INDEX "unique_schema_migrations" ON "schema_migrations" ("version");
+CREATE TABLE "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "created_at" datetime, "updated_at" datetime);
+INSERT INTO schema_migrations (version) VALUES ('20150814211733');
+
+INSERT INTO schema_migrations (version) VALUES ('20150814211741');
+
